1cff617c2897d01dcb64b6128949a32e9b813754
Daniel Reyes <danielreyesparrilla17@gmail.com>
2020-08-26 01:22:34 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-552620804cc13810ff48.js"
+    "url": "webpack-runtime-624cd62b947d102c9f98.js"
   },
   {
     "url": "framework-85aff51d15c28c7fbe5f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4bcbe8f64a8d0704ec30ad958538bd12"
+    "revision": "c877963c89f80a9d579b2ea391045275"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f27780c3f0362aadcf6f8db099189f11"
+    "revision": "9a777ac802626c9fc11d2bc7eeae2bf9"
   },
   {
     "url": "polyfill-19a11a2c8a0865319485.js"
